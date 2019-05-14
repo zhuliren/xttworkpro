3186688fad2bcdc9d5dc66d99d326d6ced30b80e
lirenzhu <1029422171@qq.com>
2019-05-15 00:04:39 +0800

    增加授信模块（未完善相关功能）

---
+++ application/index/controller/Lc.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace app\index\controller;
+
+
+class Lc
+{
+    public function myLc(){
+
+    }
+
+    public function lcHis(){
+
+    }
+
+    public function reLc(){
+
+    }
+
+    public function changeLc(){
+        
+    }
+}
